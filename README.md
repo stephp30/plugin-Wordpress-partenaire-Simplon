c3b772ea42b82b647c36d73b86b16a7d803ed9ee
stephp30 <stephan.pellet.lp2@gmail.com>
2017-12-07 17:13:47 +0100

    plugin wordpress Simplon

---
+++ README.md
@@ -0,0 +1,22 @@
+# Partenariat Simplon
+## Une Simple Extention WordPress
+
+### Description :
+
+Plugin partenaire Simplon
+
+### Installation :
+
+Compresser le PHP en ZIP et installer dans les extensions Wordpress
+
+### Utilisation :
+
+Rajouter le shortcode [simplon] dans vos articles Wordpress
+
+### Ressources :
+#### Déclaration du plugin :
+https://openclassrooms.com/courses/propulsez-votre-site-avec-wordpress/creer-des-plugins#r-1892278  
+https://developer.wordpress.org/plugins/the-basics/header-requirements/
+
+#### Shortcodes :
+https://www.sitepoint.com/wordpress-shortcodes-tutorial/ - chapitre Simple Shortcodes 
---
+++ partenariat-simplon.php
@@ -0,0 +1,22 @@
+<?php
+/*
+Plugin Name:  Simplon.co Plugin
+Plugin URI:   https://developer.wordpress.org/plugins/the-basics/
+Description:  Basic WordPress Plugin Header Comment
+Version:      20160911
+Author:       stephp
+Author URI:   https://developer.wordpress.org/
+License:      GPL2
+License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain:  wporg
+Domain Path:  /languages
+*/
+
+function PartenariatSimplon() {
+	return '<p>La publication de cet article est possible grâce à mon super partenaire simplon.co - une entreprise de
+l’économie sociale et solidaire et un réseau de « fabriques » (écoles) qui propose
+des formations GRATUITES pour devenir développeur web.</p>';
+}
+add_shortcode('simplon', 'PartenariatSimplon');
+
+
